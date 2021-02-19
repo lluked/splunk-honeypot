fb267482bd502c5702c1465e5612ebf0d2bba57c
lluked <17367626+lluked@users.noreply.github.com>
2021-02-19 18:17:41 +0000

    set host public ip.

--- opt/splunk-honeypot/bin/get-publicip.sh
+++ opt/splunk-honeypot/bin/get-publicip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-PUBLIC_IP=$(curl -s 'http://ipv4.icanhazip.com/') 
-echo PUBLIC_IP=${PUBLIC_IP} > ip.env
+PUBLIC_IP=$(curl -s 'http://ipv4.icanhazip.com/')
+sed -i 's|host = manuka|host ='${PUBLIC_IP}'|' /opt/splunk-honeypot/docker/splunk/opt/splunk/etc/apps/cowrie-splunk-app-main/local/inputs.conf
