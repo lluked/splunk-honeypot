388eb5180817ca0c088982516be164d02336d247
lluked <17367626+lluked@users.noreply.github.com>
2021-02-20 15:08:59 +0000

    public ip updates

--- opt/splunk-honeypot/bin/get-publicip.sh
+++ opt/splunk-honeypot/bin/get-publicip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PUBLIC_IP=$(curl -s 'http://ipv4.icanhazip.com/')
-if [[ "$PUBLIC_IP" =~ ^(?:(?:2(?:[0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9])\.){3}(?:(?:2([0-4][0-9]|5[0-5])|[0-1]?[0-9]?[0-9]))$ ]]; then
+if [[ $PUBLIC_IP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
   sed -i '/host = /c host = '${PUBLIC_IP}'' /home/dev/Projects/Docker/splunk-honeypot/opt/splunk-honeypot/docker/splunk/opt/splunk/etc/apps/cowrie-splunk-app-main/local/inputs.conf
 fi
